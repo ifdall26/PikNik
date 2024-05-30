c59f454b26bee489cb01dd10b95118ad7c4db589
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-31 03:48:13 +0700

    drawer button & drawer menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,13 @@
 import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/responsive.css';
+
+// event listener untuk drawer button
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerButton = document.getElementById('hamburgerButton');
+  const navMenu = document.getElementById('navMenu');
+
+  hamburgerButton.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+  });
+});
