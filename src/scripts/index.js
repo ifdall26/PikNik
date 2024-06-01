a305c1786d7be55ba912251f226d83a320ff1749
ifdall26 <lisyukri.ifdal@gmail.com>
2024-06-01 23:50:03 +0700

    menambahkan event listener untuk button login dan daftar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,16 @@
 import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/responsive.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loginButton = document.querySelector('.btn-orange');
+  const registerButton = document.querySelector('.btn-blue');
+
+  loginButton.addEventListener('click', () => {
+    window.location.href = 'login.html';
+  });
+
+  registerButton.addEventListener('click', () => {
+    window.location.href = 'register.html';
+  });
+});
