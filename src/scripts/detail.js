bb92bfcf90e1080e051bba3c0ac99b07399be133
ifdall26 <lisyukri.ifdal@gmail.com>
2024-06-12 05:05:47 +0700

    menampilkan map dari data.json

--- src/scripts/detail.js
+++ src/scripts/detail.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('heroImage').src = selectedDestinasi.gambar;
     document.getElementById('namaDestinasi').textContent = selectedDestinasi.nama_destinasi;
     document.getElementById('deskripsiDestinasi').textContent = selectedDestinasi.deskripsi;
+    document.getElementById('mapSrc').src = selectedDestinasi.mapSrc;
   } else {
     alert('Tidak ada destinasi yang dipilih');
     window.location.href = 'index.html';
