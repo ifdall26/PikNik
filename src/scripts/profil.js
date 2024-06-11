12fbb8ff39427e0e5c292a17c95446411a430358
ifdall26 <lisyukri.ifdal@gmail.com>
2024-06-11 08:41:44 +0700

    menambahkan event listener untuk berganti dari data pribadi dan pesanan saya

--- src/scripts/profil.js
+++ src/scripts/profil.js
@@ -1,1 +1,23 @@
 import '../styles/profile.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dataPribadiTab = document.getElementById('dataPribadiTab');
+  const pesananSayaTab = document.getElementById('pesananSayaTab');
+
+  const dataPribadiContent = document.getElementById('dataPribadiContent');
+  const pesananSayaContent = document.getElementById('pesananSayaContent');
+
+  dataPribadiTab.addEventListener('click', () => {
+    dataPribadiTab.classList.add('active');
+    pesananSayaTab.classList.remove('active');
+    dataPribadiContent.classList.add('active');
+    pesananSayaContent.classList.remove('active');
+  });
+
+  pesananSayaTab.addEventListener('click', () => {
+    pesananSayaTab.classList.add('active');
+    dataPribadiTab.classList.remove('active');
+    pesananSayaContent.classList.add('active');
+    dataPribadiContent.classList.remove('active');
+  });
+});
