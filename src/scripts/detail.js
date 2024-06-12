68e2b9fcb2577434519430c68d5e926cd16b3f3d
ifdall26 <lisyukri.ifdal@gmail.com>
2024-06-12 18:01:32 +0700

    menambahkan pop up untuk form booking

--- src/scripts/detail.js
+++ src/scripts/detail.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-alert */
+/* eslint-disable func-names */
+/* eslint-disable eqeqeq */
 import '../styles/detail.css';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,4 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
     alert('Tidak ada destinasi yang dipilih');
     window.location.href = 'index.html';
   }
+
+  // Get the popup
+  const popup = document.getElementById('bookingPopup');
+
+  // Get the button that opens the popup
+  const btn = document.querySelector('.booking button');
+
+  // Get the <span> element that closes the popup
+  const span = document.getElementsByClassName('close')[0];
+
+  // When the user clicks the button, open the popup
+  btn.onclick = function () {
+    popup.style.display = 'flex';
+  };
+
+  // When the user clicks on <span> (x), close the popup
+  span.onclick = function () {
+    popup.style.display = 'none';
+  };
+
+  // When the user clicks anywhere outside of the popup, close it
+  window.onclick = function (event) {
+    if (event.target == popup) {
+      popup.style.display = 'none';
+    }
+  };
 });
