350765ca4fedc9fb7cab50828f850beb92803ba2
ifdall26 <lisyukri.ifdal@gmail.com>
2024-06-12 18:04:02 +0700

    menambahkan pop up booking

--- src/scripts/detail.js
+++ src/scripts/detail.js
@@ -17,21 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
     window.location.href = 'index.html';
   }
 
-  // Get the popup
   const popup = document.getElementById('bookingPopup');
 
-  // Get the button that opens the popup
   const btn = document.querySelector('.booking button');
 
-  // Get the <span> element that closes the popup
   const span = document.getElementsByClassName('close')[0];
 
-  // When the user clicks the button, open the popup
   btn.onclick = function () {
     popup.style.display = 'flex';
   };
 
-  // When the user clicks on <span> (x), close the popup
   span.onclick = function () {
     popup.style.display = 'none';
   };
