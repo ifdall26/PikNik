82f1b6ac22739753184dd5d930196ebd47d2f574
ifdall26 <lisyukri.ifdal@gmail.com>
2024-07-11 23:12:39 +0700

    username pada halaman profil

--- src/scripts/profil.js
+++ src/scripts/profil.js
@@ -3,9 +3,26 @@ import '../styles/profile.css';
 document.addEventListener('DOMContentLoaded', () => {
   const dataPribadiTab = document.getElementById('dataPribadiTab');
   const pesananSayaTab = document.getElementById('pesananSayaTab');
-
   const dataPribadiContent = document.getElementById('dataPribadiContent');
   const pesananSayaContent = document.getElementById('pesananSayaContent');
+
+  const usernameDisplay = document.getElementById('usernameDisplay');
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  // Tambahkan input lain jika diperlukan
+
+  const userName = localStorage.getItem('userName');
+  const userEmail = localStorage.getItem('userEmail');
+  // Ambil data lain jika diperlukan
+
+  if (userName) {
+    usernameDisplay.textContent = userName;
+    nameInput.value = userName;
+  }
+
+  if (userEmail) {
+    emailInput.value = userEmail;
+  }
 
   dataPribadiTab.addEventListener('click', () => {
     dataPribadiTab.classList.add('active');
