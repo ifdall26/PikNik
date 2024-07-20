8c2110069f58e91d3a41ad153f97bdbc0cd6bc4c
ifdall26 <lisyukri.ifdal@gmail.com>
2024-07-21 03:30:59 +0700

    perbaikan autentikasi login

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const password = document.getElementById('password').value;
 
   try {
-    const response = await fetch('https://pariwisata-api-production-ffda.up.railway.app/login', {
+    const response = await fetch('https://pariwisata-api-production-ffda.up.railway.app/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
